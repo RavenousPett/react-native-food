f3730f329ed64ae6cc82d8686ef9408c032b18b9
Richard <richard@tlgd.co.uk>
2021-01-15 17:11:43 +0000

    Adding some state for the text input

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,10 +2,16 @@ import React from 'react';
 import { View, TextInput, StyleSheet } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 
-const SearchBar = () => {
+const SearchBar = ({ term, onTermChange}) => {
   return <View style={styles.backgroundStyle}>
     <Feather name="search" style={styles.iconStyle} />
-    <TextInput style={styles.inputStyle} placeholder="search" />
+    <TextInput
+      autoCapitalize="none"
+      autoCorrect={false}
+      style={styles.inputStyle}
+      placeholder="search"
+      onChangeText={newTerm => onTermChange(newTerm)}
+    />
   </View>
 };
 
